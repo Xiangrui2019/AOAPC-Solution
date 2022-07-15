f52ab3fdd224332d93cd5fe779d221e671425f5d
VHacker2022 <xiangrui@aiursoft.com>
2022-07-15 21:24:08 +0800

    p37 reverse_out.cpp

---
+++ reverse_out.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+#define MAX_N 100
+
+int numbers[MAX_N];
+
+int main()
+{
+    int x, n = 0;
+    while (scanf("%d", &x) == 1)
+        numbers[n++] = x;
+    
+    for (int k = n - 1; k >= 1; k--)
+        printf("%d ", numbers[k]);
+    printf("%d\n", numbers[0]);
+    
+    return 0;
+}
