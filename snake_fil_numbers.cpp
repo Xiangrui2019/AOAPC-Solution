c0afc2c8269f1b922c9d8e57f6f0a8b190b99946
VHacker2022 <xiangrui@aiursoft.com>
2022-07-16 21:45:07 +0800

    snake fil

---
+++ snake_fil_numbers.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <cstring>
+
+#define MAX_N 15
+int array[MAX_N][MAX_N];
+
+int main()
+{
+    memset(array, 0, sizeof(array));
+    int n, x, y, cnt = 1;
+    scanf("%d", &n);
+    x = 0, y = n - 1; // 放置填充的初始位置
+    array[x][y] = 1;
+
+    while (cnt < n * n)
+    {
+        // 向下填充
+        while (x < n - 1 && !array[x + 1][y]) array[++x][y] = ++cnt;
+        // 向左填充
+        while (y > 0 && !array[x][y - 1]) array[x][--y] = ++cnt;
+        // 向上填充
+        while (x > 0 && !array[x - 1][y]) array[--x][y] = ++cnt;
+        // 向右填充
+        while (y < n - 1 && !array[x][y + 1]) array[x][++y] = ++cnt;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            printf("%d ", array[i][j]);
+        putchar('\n');
+    }
+
+    return 0;
+}
